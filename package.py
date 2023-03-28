9035ee7ac96838ad5b21617fc3ef5de4a56b39a7
Heorhi Samushyia <30389094+brudanstudios@users.noreply.github.com>
2023-03-28 09:55:23 +0100

    Initial commit

---
+++ package.py
@@ -0,0 +1,12 @@
+name = ""
+
+version = "0.0.1"
+
+build_command = 'python -m rezutil build {root} --ignore backup'
+private_build_requires = ["rezutil"]
+
+requires = []
+
+
+def commands():
+    pass
